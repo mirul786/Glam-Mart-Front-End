bf43dcb4227883656c2dc59bd9caf9962a478eef
Mirul Ansari <mirulkhan786@gmail.com>
2023-11-02 17:43:25 +0530

    working on product api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -186,7 +186,7 @@ const Home = () => {
                         className="card-img-top"
                         alt={p.name}
                         onClick={() => {
-                          navigate(`/api/v1/products/get-single-product/${p.slug}/${p?._id}`);
+                          navigate(`/product-details/${p.slug}/${p?._id}`);
                           window.location.reload();
                         }}
                       />
